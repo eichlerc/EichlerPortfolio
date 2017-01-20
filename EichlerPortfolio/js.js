b1fce686779e941103d0d7cd0b2bb93c19ad8d0d
eichlerc <chelsey.eichler@yahoo.com>
2017-01-19 23:46:08 -0500

    Part Two Deliverable

---
+++ EichlerPortfolio/js.js
@@ -0,0 +1,13 @@
+// var mybutton = document.getElementById('yournamesubmit');
+// var message = document.getElementById('formmessage');
+//
+//
+// mybutton.onclick = function(){
+//   var username = document.getElementById('yourname');
+//   formmessage.innerHTML = ('hi' + username.value + '!!!!');
+//   return false;
+//
+// }
+
+var name = prompt("What is Your Name");
+alert("Hello " + name);
